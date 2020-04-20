e29dc5abbafb8f3851724928b944c7e494efd2e3
Mehmet Hazar Artuner <hazar.artuner@gmail.com>
2020-04-20 20:44:18 +0300

    Initial Commit

---
+++ OpenGL Demos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Demos
+//
+//  Created by Mehmet Hazar Artuner on 20.04.2020.
+//  Copyright © 2020 Mehmet Hazar Artuner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
